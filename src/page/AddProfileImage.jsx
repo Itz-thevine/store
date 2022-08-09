547b8fea92c19facbad943d8558bbd6d0c51489d
Itz-thevine <divine2christ@yahoo.com>
2022-08-09 11:22:31 +0100

    commit

--- src/page/AddProfileImage.jsx
+++ src/page/AddProfileImage.jsx
@@ -7,6 +7,7 @@ import { updateProfile } from 'firebase/auth'
 
 const AddProfileImage = () => {
     const [photoBinary, setPhotoBinary] = useState(null);
+    const [photoName, setPhotoName] = useState('Click to upload');
     const [loading, setLoading] = useState(false);
 
     // redirection
@@ -45,17 +46,19 @@ const AddProfileImage = () => {
     }
     // handle the image change
     const handleImgChange = (e) => {
+        const file = e.target.files[0];
         if (e.target.files[0]) {
             setPhotoBinary(e.target.files[0])
         }
+        setPhotoName(file.name)
     }
 
-    // console.log(photoBinary)
-
+    
     const handleImgClick = () => {
         upload(photoBinary, user, setLoading)
     }
-
+    
+    // console.log(photoBinary.name)
     
     
     
@@ -72,8 +75,7 @@ const AddProfileImage = () => {
                             <label htmlFor="dropzone-file" className="flex flex-col justify-center py-8 items-center w-96 md:w-full h-36 md:h-auto bg-gray-50 rounded-none border-2 border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
                                 <div className="flex flex-col justify-center items-center pt-5 pb-6">
                                     <svg className="mb-3 w-10 h-10 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
-                                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-                                    <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400 font-semibold">{photoName}</p>
                                 </div>
                                 <input id="dropzone-file" type="file" onChange={handleImgChange} className="hidden" />
                                 <div></div>
